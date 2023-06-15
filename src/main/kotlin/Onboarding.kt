0a963b7dae1923f5931e332bae94e1ce35346099
Just161 <bo.niehaus@gmail.com>
2023-06-15 13:18:03 +0200

    Die Rollen, die bislang als String in einer Liste gespeichert waren, sind jetzt als Klassen angelegt.

--- src/main/kotlin/Onboarding.kt
+++ src/main/kotlin/Onboarding.kt
@@ -1,5 +1,21 @@
 fun onboarding() {
     clearScreen100()
+    print(ORANGE.ON)
+    println("8888888b.  8888888b.  8888888 888     888 8888888 888      8888888888  .d8888b.   8888888888  .d8888b. ")
+    println("888   Y88b 888   Y88b   888   888     888   888   888      888        d88P  Y88b  888        d88P  Y88b")
+    println("888    888 888    888   888   888     888   888   888      888        888    888  888        Y88b.     ")
+    println("888   d88P 888   d88P   888   Y88b   d88P   888   888      8888888    888         8888888      Y888b.  ")
+    println("8888888P   8888888P     888    Y88b d88P    888   888      888        888  88888  888             Y88b.")
+    println("888        888 T88b     888     Y88o88P     888   888      888        888    888  888               888")
+    println("888        888  T88b    888      Y888P      888   888      888        Y88b  d88P  888        Y88b  d88P")
+    println("888        888   T88b 8888888     Y8P     8888888 88888888 8888888888   Y8888P88  8888888888   Y8888P  ")
+    println()
+    val textOrwell = "“All are equal, but some are more equal than others.“\n- George Orwell, Animal Farm\n\n"
+    for (char in textOrwell) {
+        print(char)
+        Thread.sleep(30)
+    }
+    print(ORANGE.OFF)
     var validInput = false
     while (!validInput) {
         println("Please choose a language:\n1: Deutsch\n2: English\n3: Türkçe\n4: Polski\n5: العربية")
@@ -44,27 +60,8 @@ fun onboarding() {
             }
         }
     }
-Thread.sleep(1500)
+    Thread.sleep(1500)
     println()
-    //clearScreen100()
-    print(ORANGE.ON)
-    println("8888888b.  8888888b.  8888888 888     888 8888888 888      8888888888  .d8888b.   8888888888  .d8888b. ")
-    println("888   Y88b 888   Y88b   888   888     888   888   888      888        d88P  Y88b  888        d88P  Y88b")
-    println("888    888 888    888   888   888     888   888   888      888        888    888  888        Y88b.     ")
-    println("888   d88P 888   d88P   888   Y88b   d88P   888   888      8888888    888         8888888      Y888b.  ")
-    println("8888888P   8888888P     888    Y88b d88P    888   888      888        888  88888  888             Y88b.")
-    println("888        888 T88b     888     Y88o88P     888   888      888        888    888  888               888")
-    println("888        888  T88b    888      Y888P      888   888      888        Y88b  d88P  888        Y88b  d88P")
-    println("888        888   T88b 8888888     Y8P     8888888 88888888 8888888888   Y8888P88  8888888888   Y8888P  ")
-    println()
-    val textOrwell = "“All are equal, but some are more equal than others.“\n- George Orwell, Animal Farm\n\n"
-    for (char in textOrwell) {
-        print(char)
-        Thread.sleep(30)
-    }
-    Thread.sleep(1000)
-    print(ORANGE.OFF)
-    //  clearScreen100()
     val welcomeIntro = chosenLanguage?.getMessage("welcomeIntro")
     if (welcomeIntro != null) {
         println(welcomeIntro.wordWrap(100))
